5664c8e21729a231de69bbd4ffa80e8d8d29276e
richie@walkbin <richie.wang@outlook.com>
2013-03-14 18:23:04 +0800

    ZhaJinHua  创建
    Signed-off-by: richie@walkbin <richie.wang@outlook.com>

---
+++ Client/project/ZhaJinHua/Classes/DataType.h
@@ -0,0 +1,19 @@
+/********************************************************************
+	created:	2013/03/14   17:35
+	filename: 	DataType.h
+	author:		richie.wang
+	
+	purpose:	
+*********************************************************************/
+#pragma once
+
+
+#define VOID                     void
+typedef char                       CHAR;
+typedef short                      SHORT;
+typedef long                       LONG;
+typedef unsigned long        DWORD;
+typedef int                          BOOL;
+typedef unsigned char        BYTE;
+typedef unsigned short       WORD;
+typedef float                       FLOAT;
