942e24cfac84127c01faa4c72054798dd4d9c002
richie@walkbin <richie.wang@outlook.com>
2013-03-22 09:36:23 +0800

    Yaoyao create
    Signed-off-by: richie@walkbin <richie.wang@outlook.com>

--- Client/project/Yaoyao/proj.win32/main.cpp
+++ Client/project/Yaoyao/proj.win32/main.cpp
@@ -17,10 +17,10 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // create the application instance
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
-    eglView->setViewName("RabbitGo@Walkbin");
-    eglView->setFrameSize(1024, 768);
+    eglView->setViewName("Yaoyao@Walkbin");
+    eglView->setFrameSize(600, 960);
     // The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
     // So we need to invoke 'setFrameZoomFactor'(only valid on desktop(win32, mac, linux)) to make the window smaller.
-    eglView->setFrameZoomFactor(0.5f);
+    eglView->setFrameZoomFactor(0.8f);
     return CCApplication::sharedApplication()->run();
 }
