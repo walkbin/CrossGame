748afd028694ac8f1b9a2a6fd1815abb94d4508e
richie@walkbin <richie.wang@outlook.com>
2013-03-23 14:06:25 +0800

    Yaoyao项目更新
    Signed-off-by: richie@walkbin <richie.wang@outlook.com>

---
+++ Client/project/Yaoyao/Classes/base/BaseScene.h
@@ -0,0 +1,45 @@
+/********************************************************************
+	Copyright (c) 2012-2013 walkbin.com
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+	created:	2013/03/22
+	filename: 	BaseScene.h
+	author:		Richie.Wang@walkbin
+	
+	purpose:	
+*********************************************************************/
+#pragma once
+
+#include "layers_scenes_transitions_nodes/CCScene.h"
+
+namespace walkbin
+{
+    class BaseScene : public cocos2d::CCScene
+    {
+    public:
+        static BaseScene *create(void);
+
+        BaseScene(void);
+        virtual ~BaseScene(void);
+
+        virtual bool init();
+        virtual void update(float dt);
+    };
+}
